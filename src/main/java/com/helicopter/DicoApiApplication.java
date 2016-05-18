82d198c1e2be7c41d7ca24a2300c6da51b82b5fc
daebeom <2daebong@naver.com>
2016-05-19 01:55:26 +0900

    update ajp setting in configuration

--- src/main/java/com/helicopter/DicoApiApplication.java
+++ src/main/java/com/helicopter/DicoApiApplication.java
@@ -27,9 +27,9 @@ public class DicoApiApplication extends WebMvcConfigurerAdapter{
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        Connector ajpConnector = new Connector("org.apache.coyote.ajp.AjpNioProtocol");
+        Connector ajpConnector = new Connector("AJP/1.3");
         ajpConnector.setProtocol("AJP/1.3");
-        ajpConnector.setPort(8009);
+        ajpConnector.setPort(9090);
         ajpConnector.setSecure(false);
         ajpConnector.setAllowTrace(false);
         ajpConnector.setScheme("http");
